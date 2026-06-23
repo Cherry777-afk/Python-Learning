60e72767c203a8d1662f4a7f4597f2ff2317e024
Cherry777-afk <justher968@gmail.com>
2026-06-23 23:36:18 +0100

    Changing menu alnguage (from Italian to English and converting from euro to pounds GB)

--- Pizza Menu'/PizzeriaDiCherry.py
+++ Pizza Menu'/PizzeriaDiCherry.py
@@ -1,37 +1,45 @@
+# User INPUT starting point 
+
+
+
 menu = """ 
-PIZZERIA DI CHERRY 
 
-Menu':                      Prezzi:
+       - CHERRY'S PIZZERIA -
 
-. MARGHERITA                |   5.00 €
-. PATATINE & WURSTEL        |   6.00 €    
-. DIAVOLA                   |   6.50 €   
-. CACIO & PEPE              |   6.00 €
-. PARMIGIANA                |   7.00 €   
-. BRESAOLA & RUCOLA         |   7.50 €
- 
 
-Dolci: 
-. TIRAMISU'                 |   4.00 €
-. TORTA PARADISO            |   4.00 €
-. TORTA BUENO               |   4.50 €
-. PANNA & FRAGOLE           |   2.50 €      
-. BANANA SPLIT              |   3.50 €
+Pizza's:                     Prices:
+
+. MARGHERITA                |   5.00 £
+. FRIES & WURSTEL           |   6.00 £    
+. DIAVOLA                   |   6.50 £   
+. CACIO PEPE                |   6.00 £
+. PARMIGIANA                |   7.00 £   
+. BRESAOLA E RUCOLA         |   7.50 £
+. FRUTTI DI MARE            |   8.00 £
+
+
+Deserts:
+. PIZZA NUTELLA             |   4.00 £ 
+. TIRAMISU'                 |   4.00 £
+. PARADISO CAKE             |   4.00 £
+. BUENO CAKE                |   4.50 £
+. CREAM & STRAWBERRIES      |   2.50 £      
+. BANANA SPLIT              |   3.50 £
 
 ************************
-  SPECIALITA' DELLA CASA:   
-. TORTA CHERRY              |   5.00 €    
+  HOUSE SPECIALS:   
+. CHERRY CAKE               |   5.00 £   
 *************************
 
 
-Bevande: 
-. ACQUA MINERALE             |   1.00 €
-. ACQUA FRIZZANTE            |   1.50 €
-. COCA COLA                  |   2.00 € 
-. SPRITE                     |   2.00 €
-. FANTA                      |   2.00 €
-. ESTATHE'                   |   1.50 €
-. MOJITO ANALCOLOLICO        |   3.00 €
+Drinks: 
+. REFIL WATER                |   FREE
+. COKE                       |   2.00 £ 
+. SPRITE                     |   2.00 £
+. FANTA                      |   2.00 £
+. ESTATHE'                   |   1.50 £
+. NON-ALCOHOLIC MOJITO       |   4.00 £
+. NON-ALCOHOLIC SPRITZ       |   4.50 £
 
 
 
