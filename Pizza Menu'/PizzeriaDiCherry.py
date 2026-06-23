515f74a5eddd6e0869d17721f9fb27bf6a5f5760
Cherry777-afk <justher968@gmail.com>
2026-06-23 22:49:28 +0100

    addind pizza selections to the menu (this will be the menu displayed to customers)

--- Pizza Menu'/PizzeriaDiCherry.py
+++ Pizza Menu'/PizzeriaDiCherry.py
@@ -1,3 +1,13 @@
 menu = """ 
+PIZZERIA DI CHERRY 
 
+Menu':                      Prezzi:
+
+. MARGHERITA                |   5.00 €
+. PATATINE & WURSTEL        |   6.00 €    
+. DIAVOLA                   |   6.50 €   
+. CACIO & PEPE              |   6.00 €
+. PARMIGIANA                |   7.00 €   
+. BRESAOLA & RUCOLA         |   7.50 €
+ 
 """
