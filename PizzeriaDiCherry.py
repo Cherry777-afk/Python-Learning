b47ef542f540c6b2cdfd298b537c89235926ec12
Cherry777-afk <justher968@gmail.com>
2026-06-24 10:46:24 +0100

    adding Welcome brief and accomodation message

---
+++ PizzeriaDiCherry.py
@@ -0,0 +1,57 @@
+menu = """ 
+
+       - CHERRY'S PIZZERIA -
+
+
+Pizza's:                     Prices:
+
+. MARGHERITA                |   5.00 £
+. FRIES & WURSTEL           |   6.00 £    
+. DIAVOLA                   |   6.50 £   
+. CACIO PEPE                |   6.00 £
+. PARMIGIANA                |   7.00 £   
+. BRESAOLA E RUCOLA         |   7.50 £
+. FRUTTI DI MARE            |   8.00 £
+
+
+Side's:
+. FRENCH FRIES              |   2.50 £
+. CURLY FRIES               |   3.00 £
+. 4 FILLED JALAPENO         |   3.50 £
+. 4 PRAWN SPIEDINI          |   4.50 £
+. FRITTO MISTO              |   7.00 £
+. SALMON TARTARE            |   8.00 £
+
+
+Deserts:
+. PIZZA NUTELLA             |   4.00 £ 
+. TIRAMISU'                 |   4.00 £
+. PARADISO CAKE             |   4.00 £
+. BUENO CAKE                |   4.50 £
+. CREAM & STRAWBERRIES      |   2.50 £      
+. BANANA SPLIT              |   3.50 £
+
+************************
+  HOUSE SPECIALS:   
+. CHERRY CAKE               |   5.00 £   
+*************************
+
+
+Drinks: 
+. REFIL WATER                |   FREE
+. COKE                       |   2.00 £ 
+. SPRITE                     |   2.00 £
+. FANTA                      |   2.00 £
+. ESTATHE'                   |   1.50 £
+. NON-ALCOHOLIC MOJITO       |   4.00 £
+. NON-ALCOHOLIC SPRITZ       |   4.50 £
+
+"""
+
+# -Welcome brief- User INPUT starting point 
+print ("Welcome to Cherry's Pizzeria!")
+name = input(str("Enter your name to see our menu: "))
+print(f"This is our menù {name}")
+print(menu)
+
+
